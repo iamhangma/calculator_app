caeccabfe04d031c322731443614c8cccbb15d69
Hangma Kandangwa <hangmakandangwa@Hangmas-MacBook-Air.local>
2023-12-06 10:22:38 +0545

    firstcommit

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:calculator_app/view/calculator_app.dart';
+import 'package:flutter/material.dart';
+ 
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const CalculatorView(),
+       
+      },
+    ),
+  );
+}
